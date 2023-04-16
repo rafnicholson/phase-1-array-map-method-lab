7103413910e2b6f18eeb32ee1afcb429b06f41e7
Ralph Nicholson <nicholson.re@gmail.com>
2023-04-15 22:17:50 -0700

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+/*
+How can we "iterate" through individual words in a string?
+  * Use the split method
+*/
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    const sent = tutorial.split(" ");
+    const capitals = sent.map(word => word[0].toUpperCase() + word.slice(1));
+    const capitalizedWords = capitals.join(" ");
+    return capitalizedWords;
+  });
+};
+
+
